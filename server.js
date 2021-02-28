87784591bc2182bba94fb9384fdeefcdc9498fdc
MehedeeAlamAnon <36200610+MehedeeAlamAnon@users.noreply.github.com>
2021-02-28 23:02:21 +0600

    11

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const express=require('express')
 const serveStatic =require('serve-static')
 const path=require('path')
+const dotenv = require("dotenv");
+
+// load env
+dotenv.config({
+    path: "./config.env",
+});
+
 
 const app=express()
 
-app.use('/',serveStatic(path.join(__dirname,'/dist')))
+// app.use('/',serveStatic(path.join(__dirname,'/dist')))
 
-const port=process.env.PORT || 8080
 
-app.listen(port)
 
-console.log(`listening on port: ${port}`)
+
+
+const port=process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+});
+
+if (process.env.NODE_ENV === 'production') {
+    // Static folder
+    app.use(express.static(__dirname + '/dist/'));
+
+    // Handle SPA
+    // app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
-// vue.config.js
-
-const path=require('path');
-
-module.exports = {
-     outputDir: path.resolve(__dirname,'server/public'),
-    devServer:{
-        proxy:{
-            '/api/':{
-                target:'http://developer.amanabigbazar.com/',
-                changeOrigin: true,
-            }
-        }
-    },
-}
+// // vue.config.js
+//
+// const path=require('path');
+//
+// module.exports = {
+//      outputDir: path.resolve(__dirname,'server/public'),
+//     devServer:{
+//         proxy:{
+//             '/api/':{
+//                 target:'http://developer.amanabigbazar.com/',
+//                 changeOrigin: true,
+//             }
+//         }
+//     },
+// }
