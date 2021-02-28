a5a4aa0b7a87d4e51e0c7c8fea55e774d7910075
MehedeeAlamAnon <36200610+MehedeeAlamAnon@users.noreply.github.com>
2021-02-28 21:54:57 +0600

    9.54pm

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 // vue.config.js
 
-const path=require('path');
+// const path=require('path');
 
 module.exports = {
-    outputDir: path.resolve(__dirname,'server/public'),
+    // outputDir: path.resolve(__dirname,'server/public'),
     devServer:{
         proxy:{
-            '/api':{
+            '/api/':{
                 target:'http://developer.amanabigbazar.com/'
             }
         }
