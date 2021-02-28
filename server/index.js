951d6180a790826062f20208f21088794830c4c0
MehedeeAlamAnon <36200610+MehedeeAlamAnon@users.noreply.github.com>
2021-02-28 20:56:11 +0600

    heroku

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express=require('express')
+const serveStatic =require('serve-static')
+const path=require('path')
+
+const app=express()
+
+// app.use('/',serveStatic(path.join(__dirname,'/dist')))
+
+if (process.env.NODE_ENV === 'production') {
+    // Static folder
+    app.use(express.static(__dirname + '/public/'));
+
+    // Handle SPA
+    // app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
+const port=process.env.PORT || 8080
+
+app.listen(port)
+
+console.log(`listening on port: ${port}`)
+
+
+
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 // vue.config.js
+
+const path=require('path');
+
 module.exports = {
+    outputDir: path.resolve(__dirname,'server/public'),
     devServer:{
         proxy: 'http://developer.amanabigbazar.com/'
     }
