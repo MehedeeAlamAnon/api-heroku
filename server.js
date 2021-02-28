c07c67447f735aaccb9bc875e307e6ba26298e1a
MehedeeAlamAnon <36200610+MehedeeAlamAnon@users.noreply.github.com>
2021-03-01 00:41:51 +0600

    12.41pm

--- server.js
+++ server.js
@@ -1,35 +1,13 @@
 const express=require('express')
 const serveStatic =require('serve-static')
 const path=require('path')
-const dotenv = require("dotenv");
-
-// load env
-dotenv.config({
-    path: "./config.env",
-});
-
 
 const app=express()
 
-// app.use('/',serveStatic(path.join(__dirname,'/dist')))
-
-
-
-// export const API = process.env.NODE_ENV === 'production' ? 'https://yourapp.herokuapp.com/api' : 'http://localhost:5000/api';
-
-
-
-if (process.env.NODE_ENV === 'production') {
-    // Static folder
-    app.use(express.static(__dirname + '/dist/'));
-
-    // Handle SPA
-    // app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-}
+app.use('/',serveStatic(path.join(__dirname,'/dist')))
 
 const port=process.env.PORT || 8080
 
-app.listen(port, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
-});
+app.listen(port)
 
+console.log(`listening on port: ${port}`)
---
+++ server3.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const morgan = require("morgan");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const serveStatic =require('serve-static')
+const path=require('path')
+
+// Create Express Server
+const app = express();
+
+app.use('/',serveStatic(path.join(__dirname,'/dist')))
+
+// Configuration
+const PORT = 8080;
+const HOST = "https://heroku-vue-api.herokuapp.com/";
+const API_SERVICE_URL = "http://developer.amanabigbazar.com";
+
+// Logging
+app.use(morgan('dev'));
+
+// Info GET endpoint
+app.get('/info', (req, res, next) => {
+    res.send('This is a proxy service which proxies to Billing and Account APIs.');
+});
+
+// Authorization
+app.use('', (req, res, next) => {
+    if (req.headers.authorization) {
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+
+// Proxy endpoints
+app.use('/api', createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+        [`^/json_placeholder`]: '/api',
+    },
+}));
+
+// Start the Proxy
+app.listen(HOST, () => {
+    // console.log(`Starting Proxy at ${HOST}:${PORT}`);
+    console.log(`Starting Proxy at ${HOST}`);
+});
